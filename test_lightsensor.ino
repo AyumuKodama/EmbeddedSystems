6d6b0a7cfe7dd7cdb87eecb89a20c81c28afc6bc
AyumuKodama <31222310+AyumuKodama@users.noreply.github.com>
2020-12-22 09:09:37 +0900

    Update test_lightsensor.ino

--- test_lightsensor.ino
+++ test_lightsensor.ino
@@ -1,17 +1,16 @@
-int buzzer = 12;                                        // the pin where you will put the LED
-int lightSense = A7;                  // the analog pin where you put your sensorLED
+int buzzer = 12;                                      
+int lightSense = A7;              
 int ledPin = 10;
 int light = 0;
 int count = 0;
-int base = 1024;
 int test = 1024;
 int test2 = 1024;
 int test3 = 1024;
 int dark = 125;
 
 
-int wait = 3;                  // the speed of the readings; a lower number is a higher speed
-int samples = 100;                   // the amount of samples to compare to make one reading
+int wait = 3;              
+int samples = 100;                   
 
 void setup() {
   pinMode(buzzer, OUTPUT);
@@ -20,22 +19,21 @@ void setup() {
 
 
 void loop() {                                            
-for(count = 0; count < samples; count++) {// remember the lowest value out of many readings
+for(count = 0; count < samples; count++) {
    light = analogRead(lightSense);
-   delay(wait);                                   // the sensor needs a delay here to catch its breath
+   delay(wait);       
    if (light < test){
-    test3 = light;                                            // remember the 3 lowest readings
+    test3 = light;                          
     test2 = test3;
     test = test2;
    }
   }
-  if (test < dark && test2 < dark && test3 < dark){//this value was taken from trial-and-error
+  if (test < dark && test2 < dark && test3 < dark){
     tone(buzzer, 2300);
     digitalWrite(ledPin, HIGH);
   } else {
     noTone(buzzer);
-    digitalWrite(ledPin, LOW);
-    base = test;                                         // if the lowest test reading is higher than the base, base will be reset
+    digitalWrite(ledPin, LOW);                                
   }
   test = 1024;
 }
